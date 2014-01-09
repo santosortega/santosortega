ddaf625d7a28c9a498a47caedb992dbd74a8b1ab
Santos Ortega <seortega13@gmail.com>
2014-01-08 16:16:03 -0800

    Added gemfile

---
+++ public/js/loadWelcome.js
@@ -0,0 +1,37 @@
+/* 
+ * Simple javascript file to get the current language from the browser
+ * and display the proper welcome in that language if posssible.
+ */
+document.write('<scr'+'ipt type="text/javascript" src="js/jquery.js" ></scr'+'ipt>');
+var lang;
+
+function getWelcome() {
+    lang = getLanguage();
+    if (!lang || !phrases[lang]) {
+        lang = 'nl';
+    }
+    document.getElementById('welcome').innerHTML = phrases[lang];
+    
+}
+
+ function getLanguage() {
+
+         if (navigator.language) {
+             lang = navigator.language;
+         } else if (navigator.userLanguage) {
+             lang = navigator.userLanguage;
+         }
+
+         if (lang && lang.length > 2) {
+             lang = lang.substring(0, 2);
+         }
+
+         return lang;
+     }
+
+var phrases = { /* translation table for page */
+    en: ["<h1>Welcome!</h1>\n\
+<p>To my site</p>\n\
+<p>\n\
+</p>"]
+};
